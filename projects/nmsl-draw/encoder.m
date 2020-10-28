19cef0e0838abcd11ae5534f4a81a8301f574a07
Aster <galaster@foxmail.com>
2020-10-28 16:13:32 +0800

    🔮 Implement an encoder

---
+++ projects/nmsl-draw/encoder.m
@@ -0,0 +1,20 @@
+singleQ = StringQ[#] && !StringContainsQ[#, "."]&;
+find[i_Integer] := Block[
+	{url, ms, fs},
+	url = <|
+		"Scheme" -> "https",
+		"Domain" -> "www.ebi.ac.uk",
+		"Path" -> {"chembl", "api", "data", "molecule"},
+		"Query" -> {
+			"format" -> "json",
+			"sort_by" -> "molecule_properties__mw_freebase",
+			"molecule_properties__mw_freebase__range" -> StringRiffle[{i, i + 1}, ","]
+		}
+	|>;
+	ms = Import[URLBuild@url, "RawJSON"]["molecules"];
+	fs = Select[#["molecule_structures", "canonical_smiles"]& /@ ms, singleQ];
+	If[ms == {}, i -> Missing[i], i -> RandomChoice@fs]
+];
+
+target = Normal@StringToByteArray["Hello world!", "UTF8"];
+output = ParallelMap[find, target];
